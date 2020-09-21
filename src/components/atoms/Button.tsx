23766513dc37e5a8e15f9735fd758b125260265c
Terai Daiki <daikiterai@gmail.com>
2020-09-22 01:03:43 +0900

    commit

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,7 +1,12 @@
 import React, { CSSProperties } from 'react'
 
 const buttonStyles: CSSProperties = {
+  fontSize: 18,
   fontWeight: 'bold',
+  background: 'green',
+  color: 'white',
+  borderRadius: 4,
+  boxShadow: '2px 2px 4px',
 }
 
 const Button = (props: {
